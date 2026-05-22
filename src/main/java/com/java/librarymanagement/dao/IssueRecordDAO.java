3c712c866a4f8f7d683af1ce55521addfb7fca02
rushikeshphad <rushikeshphad07@gmail.com>
2026-05-22 21:06:52 +0530

    added IssueRecordDAO code

--- src/main/java/com/java/librarymanagement/dao/IssueRecordDAO.java
+++ src/main/java/com/java/librarymanagement/dao/IssueRecordDAO.java
@@ -1,5 +1,20 @@
 package com.java.librarymanagement.dao;
 
-public interface IssueRecordDAO {
+import java.util.List;
 
+import com.java.librarymanagement.model.IssueRecord;
+
+public interface IssueRecordDAO 
+{
+	boolean issueBook(IssueRecord record);
+	
+	boolean returnBook(int issueId);
+	
+	IssueRecord getIssueById(int id);
+	
+	List<IssueRecord> getAllIssuedBooks();
+	
+	List<IssueRecord> getIssuesByUserId(int userId);
+	
+	List<IssueRecord> getPendingReturns();
 }
