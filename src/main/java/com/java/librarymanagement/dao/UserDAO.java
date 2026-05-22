137a949f764b2a347f9f3d24727db10cbafabffa
rushikeshphad <rushikeshphad07@gmail.com>
2026-05-22 21:02:00 +0530

    added UserDAO code

--- src/main/java/com/java/librarymanagement/dao/UserDAO.java
+++ src/main/java/com/java/librarymanagement/dao/UserDAO.java
@@ -1,5 +1,20 @@
 package com.java.librarymanagement.dao;
 
-public interface UserDAO {
+import java.util.List;
 
+import com.java.librarymanagement.model.User;
+
+public interface UserDAO 
+{
+	boolean addUser(User user);
+	
+	User getUserByEmail(String email);
+	
+	User getUserById(int id);
+	
+	List<User> getAllUsers();
+	
+	boolean updateUser(User user);
+	
+	boolean deleteUser(int id);
 }
