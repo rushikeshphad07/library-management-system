0778dbdd98ee530d9d5c2d3280050a524de14998
rushikeshphad <rushikeshphad07@gmail.com>
2026-06-09 14:03:07 +0530

    added UserService code

--- src/main/java/com/java/librarymanagement/service/UserService.java
+++ src/main/java/com/java/librarymanagement/service/UserService.java
@@ -1,5 +1,22 @@
 package com.java.librarymanagement.service;
 
-public interface UserService {
+import java.util.List;
 
+import com.java.librarymanagement.model.User;
+
+public interface UserService 
+{
+	boolean addUser(User user);
+	
+	User getUserById(int id);
+	
+	User getUserByEmail(String email);
+	
+	List<User> getAllUsers();
+	
+	boolean updateUser(User user);
+	
+	boolean deleteUser(int id);
+	
+	boolean login(String email, String password);
 }
