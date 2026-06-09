8d1edb32fc2d781efcd2e25c0546b1587f8e3c42
rushikeshphad <rushikeshphad07@gmail.com>
2026-06-09 09:02:25 +0530

    added service structure

---
+++ src/main/java/com/java/librarymanagement/service/UserService.java
@@ -0,0 +1,5 @@
+package com.java.librarymanagement.service;
+
+public interface UserService {
+
+}
---
+++ src/main/java/com/java/librarymanagement/service/impl/BookServiceImpl.java
@@ -0,0 +1,5 @@
+package com.java.librarymanagement.service.impl;
+
+public class BookServiceImpl {
+
+}
